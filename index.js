02e72566cebba710d858948ddc96c2012fbf08bc
nxNghia <nghia1421999@gmail.com>
2022-01-09 02:55:40 +0700

    fix connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const con = require('./database')
+const { con } = require('./database')
 const axios = require('axios')
 
 const port = process.env.PORT || 8000
